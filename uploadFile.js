bd839f84650455af25d8a2750818325c79224e96
Arturo Ruiz <gregory.arturo.ruiz@gmail.com>
2024-09-10 01:05:17 -0400

    refactor: Update file paths in package.json and upload.js

--- uploadFile.js
+++ uploadFile.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const drive = require("./googleDrive");
 
 const fs = require("fs");
@@ -7,12 +5,6 @@ const fs = require("fs");
 const { PassThrough } = require("stream");
 
 const mime = require("mime-types");
-
-const jsonData = require("./files.json");
-
-const { deleteFileByNameInFolder } = require("./deleteFileByNameInFolder");
-
-const folderId = process.env.FOLDER_ID;
 
 /**
  * Uploads a file to Google Drive.
@@ -65,32 +57,4 @@ async function uploadFile(filePath, fileName, folderId) {
   }
 }
 
-/**
- * Uploads multiple files from a JSON array.
- * @param {Array} jsonData - The JSON array containing file data.
- * @param {string} folderId - The ID of the folder to upload the files to.
- */
-async function uploadFilesFromJSON(jsonData, folderId) {
-  try {
-    // Iterate over each file data in the JSON array
-    for (const fileData of jsonData) {
-      const { filePath, fileName } = fileData;
-
-      // Delete the previous file with the same name in the folder
-      await deleteFileByNameInFolder(folderId, fileName);
-
-      console.log("Deleting previous file:", folderId, fileName);
-
-      // Upload the current file to Google Drive
-      await uploadFile(filePath, fileName, folderId);
-
-      console.log("Uploading file:", fileName, folderId, filePath);
-    }
-
-    console.log("All files have been uploaded.");
-  } catch (err) {
-    console.error("Error uploading files from JSON:", err);
-  }
-}
-
-uploadFilesFromJSON(jsonData, folderId);
+module.exports = { uploadFile };
---
+++ uploadFilesFromJson.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const jsonData = require("./files.json");
+
+const { deleteFileByNameInFolder } = require("./deleteFileByNameInFolder");
+
+const { uploadFile } = require("./uploadFile");
+
+const folderId = process.env.FOLDER_ID;
+
+/**
+ * Uploads multiple files from a JSON array.
+ * @param {Array} jsonData - The JSON array containing file data.
+ * @param {string} folderId - The ID of the folder to upload the files to.
+ */
+async function uploadFilesFromJson(jsonData, folderId) {
+  try {
+    // Iterate over each file data in the JSON array
+    for (const fileData of jsonData) {
+      const { filePath, fileName } = fileData;
+
+      // Delete the previous file with the same name in the folder
+      await deleteFileByNameInFolder(folderId, fileName);
+
+      console.log("Deleting previous file:", folderId, fileName);
+
+      // Upload the current file to Google Drive
+      await uploadFile(filePath, fileName, folderId);
+
+      console.log("Uploading file:", fileName, folderId, filePath);
+    }
+
+    console.log("All files have been uploaded.");
+  } catch (err) {
+    console.error("Error uploading files from JSON:", err);
+  }
+}
+
+uploadFilesFromJson(jsonData, folderId);
